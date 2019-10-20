a1da72259ce048b395d7bc1472eda7ed247e4479
Jesús Torres <jesustc17@gmail.com>
2019-10-19 21:43:30 -0400

    Aplicando middleware basico y bodyParser

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const hostname = '127.0.0.1'; 
 const port = 3000;
+
+// se puedne agregar varios archivos
+// app.use('/static', express.static('public'))
+app.use(express.static('public'))
+
+// middleware bodyParser 
+app.use(bodyParser.json()); // para leer parametros json application/json
+app.use(bodyParser.urlencoded({extended: true}) ); // 
 
 app.set('view engine', 'pug');
 
@@ -11,6 +20,14 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login');
+})
+
+app.post('/users', (req, res) => {
+    const {email, password} = req.body
+    console.log('email: ', email);
+    console.log('password: ', password);
+    
+    res.send('Datos recibidos');
 })
 
 app.listen(port, hostname, () => {

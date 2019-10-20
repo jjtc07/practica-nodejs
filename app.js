a9993d183071e93cde23cd5399e3cddb362f3efe
Jesús Torres <jesustc17@gmail.com>
2019-10-20 09:42:33 -0400

    inicios con mongoose y mongodb

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const app = express();
-const hostname = '127.0.0.1'; 
+const hostname = '127.0.0.1';
 const port = 3000;
+
+mongoose.connect('mongodb://localhost/fotos', { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+const userSchemaJSON = {
+    email:String,
+    password:String
+}
+
+const user_schema = Schema(userSchemaJSON);
+const User = mongoose.model('User', user_schema);
+
 
 // se puedne agregar varios archivos
 // app.use('/static', express.static('public'))
@@ -19,15 +36,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.render('login');
+
+    User.find((err, users) => {
+        if (err) return console.error(err);
+        console.log('usuarios: ', users);
+        res.render('login');
+    })
+    
+    // res.render('login');
 })
 
 app.post('/users', (req, res) => {
     const {email, password} = req.body
     console.log('email: ', email);
     console.log('password: ', password);
+
+    const user = new User({email: email, password, password});
+
+    user.save().then(() => {
+        res.send('Guardamos tus datos');
+    });
     
-    res.send('Datos recibidos');
+    // res.send('Datos recibidos');
 })
 
 app.listen(port, hostname, () => {
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// tipos de datos para guardar en mongodb
+/*
+  String
+  Number
+  Date
+  Buffer
+  Boolean
+  Mixed
+  Objectid
+  Array
+*/

23404ddb2c754be7aae91500096a977106ff899f
Jesús Torres <jesustc17@gmail.com>
2019-10-19 20:55:03 -0400

    Aplicando layout basico con pug/jade

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'; 
 const port = 3000;
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 app.listen(port, hostname, () => {

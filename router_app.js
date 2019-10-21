c624862851c6c4c83e7bb5cee1510e736e356e81
Jesús Torres <jesustc17@gmail.com>
2019-10-20 21:37:57 -0400

    Creando y organizando rutas para arquitectura rest

--- router_app.js
+++ router_app.js
@@ -7,4 +7,33 @@ router.get('/', (req, res) => {
   // res.render('login');
 })
 
+
+router.get('/imagenes/new', (req, res) => { // create
+  res.render('app/imagenes/new');
+});
+
+router.get('/imagenes/:id/edit', (req, res) => { // edit
+
+});
+
+router.route('/imagenes/:id')
+      .get((req, res) => { // show
+
+      })
+      .put((req, res) => { // update
+
+      })
+      .delete((req, res) => { // delete
+
+      })
+
+router.route('/imagenes')
+      .get((req, res) => { // index
+
+      })
+      .post((req, res) => { // store
+
+      })
+
+
 module.exports = router;
